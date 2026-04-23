18e0999d8623a8915d536d278ed209766a5fb273
Daniel Mann <dandamann312@gmail.com>
2026-04-23 00:16:13 +0000

    fix: when moving to the rsvp page, clear the anchor link from the url

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <>
-      <Nav onRsvpClick={() => setView('rsvp-lookup')} view={view} onNavigateToLanding={() => setView('landing')} />
+      <Nav onRsvpClick={() => { history.replaceState(null, '', window.location.pathname); setView('rsvp-lookup') }} view={view} onNavigateToLanding={() => setView('landing')} />
       {view === 'landing' && (
         <main className="max-w-2xl mx-auto w-full">
           <Hero />
