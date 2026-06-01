4f1300580ff55fe74f98b4653620090a2627f62f
Daniel Mann <dandamann312@gmail.com>
2026-06-01 07:51:51 +0000

    fix: added newlines to text

--- src/components/sections/DressCodeParking.tsx
+++ src/components/sections/DressCodeParking.tsx
@@ -15,21 +15,24 @@ export default function DressCodeParking() {
 
       {/* Left: stacked boxes */}
       <div className="flex-1 self-center flex flex-col gap-12">
-        <div id="dress-code" className="border border-white/25 px-4 p-4 text-center">
+        <div id="dress-code" className="border border-white/25 p-4 text-center">
           <p className="text-base font-bold uppercase tracking-widest mb-2">Dress Code</p>
           <p className="text-lg mb-1">Semi-formal.</p>
           <p className="text-lg">We suggest wearing something suitable for a summer garden wedding, with comfortable footwear that won't sink in the grass.</p>
         </div>
 
-        <div id="parking-transportation" className="border border-white/25 px-4 p-4 text-center">
+        <div id="parking-transportation" className="border border-white/25 p-4 text-center">
           <p className="text-base font-bold uppercase tracking-widest mb-2">Parking &amp; Transportation</p>
-          <p className="text-lg mb-1">There is plenty of parking available onsite. Cars may be left overnight and collected by 11am the following day.</p>
-          <p className="text-lg">Taxis and rideshares are available in the area. We recommend booking them before you are ready to leave — they can be difficult to get at short notice in Kumeū.</p>
+          <p className="text-lg">There is plenty of parking available onsite.</p>
+          <p className="text-lg mb-1">Cars may be left overnight and collected by 11am the following day.</p>
+          <p className="text-lg">Taxis and rideshares are available in the area. </p>
+          <p className="text-lg">We recommend booking them before you are ready to leave - they can be difficult to get at short notice in Kumeū.</p>
         </div>
 
-        <div id="gifts" className="border border-white/25 px-4 p-4 text-center">
+        <div id="gifts" className="border border-white/25 p-4 text-center">
           <p className="text-base font-bold uppercase tracking-widest mb-2">Gifts</p>
-          <p className="text-lg">Your company on our special day is all we need. However, should you wish to contribute to our honeymoon, we will have a wishing well at the venue.</p>
+          <p className="text-lg">Your company on our special day is all we need.</p>
+          <p className="text-lg">However, should you wish to contribute to our honeymoon, we will have a wishing well at the venue.</p>
         </div>
       </div>
 
