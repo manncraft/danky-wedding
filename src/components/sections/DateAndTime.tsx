ee61145eb99420febec43c06493e8d9cce5ef921
Daniel Mann <dandamann312@gmail.com>
2026-06-28 03:08:46 +0000

    feat: fancy italic on the "about the ceremony" heading

--- src/components/sections/DateAndTime.tsx
+++ src/components/sections/DateAndTime.tsx
@@ -6,7 +6,7 @@ export default function DateAndTime() {
       <div className="w-full sm:w-[35%] flex flex-col overflow-hidden">
         <div className="leading-tight shrink-0">
           <p className="accent text-3xl font-light">About the</p>
-          <p className="fancy text-6xl pb-2">Ceremony</p>
+          <p className="fancy-italic text-6xl pb-2">Ceremony</p>
         </div>
         <div className="flex-1 min-h-0 hidden sm:flex items-center justify-center mt-4">
           <img src="/img/rose3.png" alt="" className="max-h-[28vh] max-w-full object-contain" />
